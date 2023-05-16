18983085721f64d18910dffe7b7f3f85e3374815
Kanhaiya2580 <Kanhaiya@gomilestone.com>
2023-05-16 11:26:49 +0530

    add version

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -6,8 +6,19 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
+use PragmaRX\Version\Package\Facade as Version;
 
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    public function index()
+    {
+        $version = Version::version(); // Get the version information
+        $version = Version::format('compact'); // Get the version information
+        dd($version);
+        // Use the version information as needed
+
+        // return view('your-view', ['version' => $version]);
+    }
 }
